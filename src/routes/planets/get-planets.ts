42f5f9aad1f64e386c9b97e30cb8019c9bb7ed3d
Isaque de Sousa <isaque.error404@gmail.com>
2025-09-10 16:47:30 -0300

    refactor: add validation and query params

--- src/routes/planets/get-planets.ts
+++ src/routes/planets/get-planets.ts
@@ -1,7 +1,18 @@
+import { and, asc, ilike } from "drizzle-orm";
 import { FastifyPluginAsync } from "fastify";
 import z from "zod";
 import { db } from "../../database/client";
 import { planets } from "../../database/schema";
+
+const querySchema = z.object({
+  search: z.string().optional(),
+  orderBy: z
+    .enum(["name", "id", "diameter", "population"])
+    .optional()
+    .default("id"),
+  page: z.coerce.number().optional().default(1),
+  limit: z.coerce.number().optional().default(10),
+});
 
 export const GetPlanetsRoute: FastifyPluginAsync = async (server) => {
   server.get(
@@ -10,6 +21,7 @@ export const GetPlanetsRoute: FastifyPluginAsync = async (server) => {
       schema: {
         tags: ["Planets"],
         summary: "Get all planets",
+        querystring: querySchema,
         response: {
           200: z.object({
             planets: z.array(
@@ -31,8 +43,23 @@ export const GetPlanetsRoute: FastifyPluginAsync = async (server) => {
       },
     },
     async (request, reply) => {
-      const result = await db.select().from(planets);
-      return { planets: result };
+      const { search, page, orderBy, limit } = querySchema.parse(request.query);
+
+      const conditions = search ? [ilike(planets.name, `%${search}%`)] : [];
+
+      const [result, total] = await Promise.all([
+        db
+          .select()
+          .from(planets)
+          .where(and(...conditions))
+          .orderBy(asc(planets[orderBy]))
+          .limit(limit)
+          .offset((page - 1) * limit),
+
+        db.$count(planets, and(...conditions)),
+      ]);
+
+      return { planets: result, total };
     }
   );
 };
