1b5a154ede87c98de1c09a860deaccc6b94aaef5
Isaque de Sousa <isaque.error404@gmail.com>
2025-09-10 14:57:42 -0300

    feat: add docs and new route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import fastifyStatic from "@fastify/static";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import path from "node:path";
 import { GetCharacters } from "./routes/characters/get-characters";
+import { GetCharactersById } from "./routes/characters/get-characters-by-id";
 
 const server = fastify({
   logger: {
@@ -23,25 +27,39 @@ const server = fastify({
 server.setSerializerCompiler(serializerCompiler);
 server.setValidatorCompiler(validatorCompiler);
 
-// if (process.env.NODE_ENV === "development") {
-//   server.register(fastifySwagger, {
-//     openapi: {
-//       info: {
-//         title: "Node.js API",
-//         version: "1.0.0",
-//       },
-//     },
-//     transform: jsonSchemaTransform,
-//   });
+// Swagger only in development
+if (process.env.NODE_ENV === "development") {
+  // Registers OpenAPI spec generator
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: "Star Wars API",
+        description:
+          "API for Star Wars characters, planets, vehicles, starships, and films",
+        version: "1.0.0",
+      },
+    },
+    transform: jsonSchemaTransform,
+  });
 
-//   server.register(scalarAPIReference, {
-//     routePrefix: "/docs",
-//   });
-// }
+  // Registers Swagger UI
+  server.register(fastifySwaggerUi, {
+    routePrefix: "/docs",
+    uiConfig: {
+      docExpansion: "list",
+      deepLinking: false,
+    },
+    staticCSP: true,
+    transformSpecification: (swaggerObject) => swaggerObject,
+  });
+
+  console.log("Swagger docs available at: http://localhost:3333/docs");
+}
 server.register(fastifyStatic, {
   root: path.join(__dirname, "../public"),
   prefix: "/public/",
 });
 server.register(GetCharacters);
+server.register(GetCharactersById);
 
 export { server };
