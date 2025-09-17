84eb529500f4189cc92fa84154fb038d461ce816
Isaque de Sousa <isaque.error404@gmail.com>
2025-09-17 15:40:08 -0300

    refactor: refactor organize services

---
+++ src/routes/characters/character.services.ts
@@ -0,0 +1,48 @@
+import { and, asc, eq, ilike } from "drizzle-orm";
+import z from "zod";
+import { db } from "../../database/client";
+import { characters } from "../../database/schema";
+import {
+  characterIdParamSchema,
+  characterQueryParamsSchema,
+  createCharacterBodySchema,
+} from "./character.schemas";
+
+export async function createCharacter(
+  data: z.infer<typeof createCharacterBodySchema>
+) {
+  const [newCharacter] = await db.insert(characters).values(data).returning();
+  return newCharacter;
+}
+
+export async function getCharacters(
+  query: z.infer<typeof characterQueryParamsSchema>
+) {
+  const { search, limit, orderBy, page } = query;
+
+  const conditions = search ? [ilike(characters.name, `%${search}%`)] : [];
+
+  const [result, total] = await Promise.all([
+    db
+      .select()
+      .from(characters)
+      .where(and(...conditions))
+      .orderBy(asc(characters[orderBy]))
+      .limit(limit)
+      .offset((page - 1) * limit),
+
+    db.$count(characters, and(...conditions)),
+  ]);
+
+  return { characters: result, total };
+}
+
+export async function getCharacterById(
+  characterId: z.infer<typeof characterIdParamSchema>["id"]
+) {
+  const result = await db
+    .select()
+    .from(characters)
+    .where(eq(characters.id, characterId));
+  return result[0] ?? null;
+}
