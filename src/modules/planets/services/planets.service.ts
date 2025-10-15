21034f1783c60ecddade77a503c2492c62a42260
Isaque de Sousa <isaque.error404@gmail.com>
2025-10-15 09:26:45 -0300

    feat(planets): create new services, update, delete and create

--- src/modules/planets/services/planets.service.ts
+++ src/modules/planets/services/planets.service.ts
@@ -1,5 +1,6 @@
 import { PlanetRepository } from "../repositories/planets.repository";
 import {
+  CreatePlanetData,
   Planet,
   PlanetQueryParams,
   PlanetsResponse,
@@ -30,5 +31,25 @@ export class PlanetService {
 
   async getPlanetById(id: number): Promise<Planet | null> {
     return await this.planetsRepository.findById(id);
+  }
+
+  async createPlanet(data: CreatePlanetData): Promise<Planet> {
+    return await this.planetsRepository.create(data);
+  }
+
+  async updatePlanet(
+    id: number,
+    data: Partial<CreatePlanetData>
+  ): Promise<Planet | null> {
+    const existingPlanet = await this.planetsRepository.exists(id);
+    if (!existingPlanet) {
+      return null;
+    }
+
+    return await this.planetsRepository.update(id, data);
+  }
+
+  async deletePlanet(id: number): Promise<boolean> {
+    return await this.planetsRepository.delete(id);
   }
 }
