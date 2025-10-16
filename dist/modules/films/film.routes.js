84869c97334b6c41b786f14d77d47aa3041db911
Isaque de Sousa <isaque.error404@gmail.com>
2025-10-16 19:56:43 -0300

    chore: delete dist

--- dist/modules/films/film.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filmRoutes = void 0;
-const film_handler_1 = require("./film.handler");
-const filmRoutes = async (server) => {
-    server.register(film_handler_1.getFilmsHandler);
-    server.register(film_handler_1.getFilmByIdHandler);
-};
-exports.filmRoutes = filmRoutes;
--- dist/modules/starships/starship.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.starshipRoutes = void 0;
-const starship_handler_1 = require("./starship.handler");
-const starshipRoutes = async (server) => {
-    server.register(starship_handler_1.getStarshipsHandler);
-    server.register(starship_handler_1.getStarshipByIdHandler);
-};
-exports.starshipRoutes = starshipRoutes;
