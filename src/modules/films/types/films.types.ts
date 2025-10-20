53520d72069801199da12be767e9cd83a5584e22
Isaque de Sousa <isaque.error404@gmail.com>
2025-10-20 19:21:10 -0300

    feat: create types films

---
+++ src/modules/films/types/films.types.ts
@@ -0,0 +1,28 @@
+import z from "zod";
+import {
+  createFilmSchema,
+  filmIdParamSchema,
+  filmQueryParamsSchema,
+  filmsSchema,
+} from "../schemas/films.schema";
+
+export type Film = z.infer<typeof filmsSchema>;
+export type CreateFilmData = z.infer<typeof createFilmSchema>;
+export type FilmQueryParams = z.infer<typeof filmQueryParamsSchema>;
+export type FilmIdParam = z.infer<typeof filmIdParamSchema>;
+
+export interface FilmsResponse {
+  films: Film[];
+  total: number;
+}
+
+export interface FilmResponse {
+  film: Film;
+}
+
+export interface FilmFilters {
+  search?: string;
+  limit?: number;
+  offset?: number;
+  orderBy?: keyof Film;
+}
